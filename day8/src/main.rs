60c9d505a4de11be7cd3c7ff152a1f388c85aa1a
Jorge Brown <j.brown@moovit-sp.com>
2023-12-08 14:55:41 +0100

    Add 8-2 that does not assume the input is cyclical

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -18,14 +18,33 @@ fn solve1(input: &str) -> u64 {
     moves
 }
 
+fn solve2(input: &str) -> u64 {
+    let map = Map::from(input).as_linked();
+
+    let mut moves = 0;
+    let mut current_locations = map.get_starting_locations();
+
+    for instruction in map.instructions.iter().cycle() {
+        for i in 0..current_locations.len() {
+            current_locations[i] = map.get_location(current_locations[i], instruction);
+        }
+        moves += 1;
+        if map.are_all_ending_locations(&current_locations) {
+            break;
+        }
+    }
+
+    moves
+}
+
 fn main() {
     let s = include_str!("../input.txt");
-    println!("{}", solve1(s));
+    println!("{}, {}", solve1(s), solve2(s));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::solve1;
+    use crate::{solve1, solve2};
 
     #[test]
     fn part11() {
@@ -37,5 +56,11 @@ mod tests {
     fn part12() {
         let s = include_str!("../test-input-2.txt");
         assert_eq!(solve1(s), 6);
+    }
+
+    #[test]
+    fn part2() {
+        let s = include_str!("../test-input-3.txt");
+        assert_eq!(solve2(s), 6);
     }
 }
--- day8/src/map.rs
+++ day8/src/map.rs
@@ -1,6 +1,7 @@
 use core::panic;
 use std::collections::HashMap;
 
+#[derive(Clone)]
 pub enum Instruction {
     Left,
     Right,
@@ -11,12 +12,62 @@ pub struct Map {
     locations: HashMap<String, (String, String)>,
 }
 
+pub struct LinkedMap {
+    pub instructions: Vec<Instruction>,
+    starting_locations: Vec<usize>,
+    locations: Vec<(String, (usize, usize))>,
+}
+impl LinkedMap {
+    pub fn get_location(&self, start: usize, instruction: &Instruction) -> usize {
+        match instruction {
+            Instruction::Left => self.locations[start].1.0,
+            Instruction::Right => self.locations[start].1.1,
+        }
+    }
+
+    pub fn are_all_ending_locations(&self, locations: &[usize]) -> bool {
+        locations.iter().all(|location| {
+            &self.locations[*location].0[2..3] == "Z"
+        })
+    }
+
+    pub fn get_starting_locations(&self) -> Vec<usize> {
+        self.starting_locations.clone()
+    }
+}
+
 impl Map {
     pub fn get_location(&self, start: &str, instruction: &Instruction) -> &str {
         match instruction {
             Instruction::Left => &self.locations.get(start).unwrap().0,
             Instruction::Right => &self.locations.get(start).unwrap().1,
         }
+    }
+
+    pub fn as_linked(&self) -> LinkedMap {
+        let mut locations = Vec::with_capacity(self.locations.len());
+        let mut starting_locations = Vec::new();
+
+        let mut indices: HashMap<String, usize> = HashMap::new();
+
+        for k in self.locations.keys() {
+            indices.insert(k.clone(), locations.len());
+            if &k[2..] == "A" {
+                starting_locations.push(locations.len());
+            }
+            locations.push((k.clone(), (0, 0)));
+        }
+
+        for k in self.locations.keys() {
+            let idx = indices.get(k).unwrap();
+
+            let (left, right) = self.locations.get(k).unwrap();
+
+            locations.get_mut(*idx).unwrap().1 = (*indices.get(left).unwrap(), *indices.get(right).unwrap());
+        }
+
+
+        LinkedMap { instructions: self.instructions.clone(), starting_locations, locations }
     }
 }
 
